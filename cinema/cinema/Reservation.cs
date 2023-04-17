365226355d87abc7402358fe3db5b991a761c1ee
Hlebsmaslo <94301600+Hlebsmaslo@users.noreply.github.com>
2023-04-17 23:18:06 +0300

    Add files via upload

---
+++ cinema/cinema/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace cinema
+{
+    public class Reservation
+    {
+        public int OrderCode { get; set; }
+        public int id { get; set; }
+        public Reservation(int orderCode)
+        {
+            OrderCode = orderCode;
+        }
+    }
+
+}
